075650829733af7648ddc5157b50c1d073999899
Matthew Ragan <raganmd@gmail.com>
2019-10-29 16:40:22 -0700

    changes to save tox - now uses ctrl + w to save tox, ctrl + s saves toe file update to project extenstion update to toe file update to output tox

--- day1/projectEXT.py
+++ day1/projectEXT.py
@@ -4,22 +4,28 @@ class Project:
         self.MyOp = myOp
         self.Role = myOp.par.Role
 
+        # confirm in text port that we've inited the project class
         print("Project init from {}".format(myOp))
 
     def RoleColor(self, role):
+        # color dict
         color = {
             'controller'    : (0.117, 0.0, 0.13),
             'draw'          : (0.0, 0.114, 0.020),
             'default'       : (0.1, 0.105, 0.112)
         }
+
+        # our default color is in the color dict, and is the default bg color of TD
         default = color.get('default')
         return color.get(role, default)
 
-    def ChangeBg(self, role):
+    def ChangeBg(self, role=''):
+        # changes the bg of TD based on the role
         color = self.RoleColor(role)
         ui.colors['worksheet.bg'] = color
     
     def ConfigureProject(self, role):
+        # this is our configuration order of operations
         self.ChangeBg(role)
         op('container_output').Setup(role)
     
